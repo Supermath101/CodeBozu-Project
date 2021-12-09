7e2ec4b62581f29bb6dc9fbe06b0b59cb1c15606
Supermath101 <>
2021-12-09 16:56:22 -0500

    Found Spouses in a better way

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ print("Birthday: " + soup.find(class_="bday").text)
 
 # print(soup.find(string="Spouse(s)").parent.parent.prettify())
 
+spouses = []
+
 container=soup.find(string="Spouse(s)")
-for thing in container.parent.parent.find(class_="infobox-data").stripped_strings:
-    if (re.findall("[a-zA-Z]"))
+for maybe_name in container.parent.parent.find(class_="infobox-data").stripped_strings:
+    for name in re.findall("^.+ .+$", maybe_name, re.M):
+        spouses.append(name)
+
+print("Spouses: " + ", ".join(spouses))
