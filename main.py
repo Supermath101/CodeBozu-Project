86f4c7fd61197cba43122b6f8488e6bfe4df1563
Supermath101 <>
2021-12-09 16:08:38 -0500

    Found Birthday by Scrapping

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-html_file=requests.get('https://en.wikipedia.org/wiki/Donald_Trump')
 
+html_file=requests.get('https://en.wikipedia.org/wiki/Donald_Trump')
 soup = BeautifulSoup(html_file.text, "lxml")
-print("The title: " + soup.prettify())
+
+print("Birthday: " + soup.find(class_="bday").text)
