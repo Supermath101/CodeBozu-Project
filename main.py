30772ab560e7e1d6bb85dc2c7fdde890260b5f47
Tanishqqq19 <tanmeup195@gmail.com>
2021-12-10 00:08:02 +0300

    spouse find
    I found the spouse programmily.

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ import requests
 html_file=requests.get('https://en.wikipedia.org/wiki/Donald_Trump')
 
 soup = BeautifulSoup(html_file.text, "lxml")
-print("The title: " + soup.prettify())
+html_tag=soup.find('div',class_='plainlist')
+html_tag1=soup.find('div',style='display:inline;white-space:nowrap;')
+html_tag2=soup.find('div',style='display:inline-block;line-height:normal;margin-top:1px;white-space:normal;')
+print(html_tag2.text)
