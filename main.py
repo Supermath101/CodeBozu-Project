4cb23b56de1612b29230c3a5d5ba4c52daa5da3b
Supermath101 <>
2021-12-09 16:37:29 -0500

    WIP on Donald Trump Spouses

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 html_file=requests.get('https://en.wikipedia.org/wiki/Donald_Trump')
 soup = BeautifulSoup(html_file.text, "lxml")
 
 print("Birthday: " + soup.find(class_="bday").text)
 
-html_tag=soup.find('div',class_='plainlist')
-html_tag1=soup.find('div',style='display:inline;white-space:nowrap;')
-html_tag2=soup.find('div',style='display:inline-block;line-height:normal;margin-top:1px;white-space:normal;')
-print(html_tag2.text)
+# print(soup.find(string="Spouse(s)").parent.parent.prettify())
+
+container=soup.find(string="Spouse(s)")
+for thing in container.parent.parent.find(class_="infobox-data").stripped_strings:
+    if (re.findall("[a-zA-Z]"))
