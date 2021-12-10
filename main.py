80e2d18bde3c357f1e9f3a109045dc143fd2dedd
Tanishqqq19 <tanmeup195@gmail.com>
2021-12-10 23:05:47 +0300

    I thiink I got all the information for the csv file
    There are two bugs. I would appreciate if someone helps. I put  comments on them

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ html_file=requests.get('https://en.wikipedia.org/wiki/Donald_Trump')
 soup = BeautifulSoup(html_file.text, "lxml")
 
 # Birthday
-print("Birthday: " + soup.find(class_="bday").text)
-
+date_of_birth=soup.find(class_="bday").text
+# print("Birthday: " + soup.find(class_="bday").text)
+"""
+We need it in this type of format 
+July 15,1975
+"""
 # Spouse
 spouses = []
 
@@ -16,7 +20,23 @@ for maybe_name in container.parent.parent.find(class_="infobox-data").stripped_s
     for name in re.findall("^.+ .+$", maybe_name, re.M):
         spouses.append(name)
 
-print("Spouses: " + ", ".join(spouses))
+# print("Spouses: " + ", ".join(spouses))
+
+
 # Political Party
-political_party=soup.find('div',class_='plainlist')
-print(political_party.text)
+political_party=soup.find('div',class_='plainlist').text
+
+# Politicians
+politician=soup.find('div',class_='fn').text
+
+# Birth Full name
+birth_full_name=soup.find('div',class_="nickname").text
+
+# Birth Location
+"""
+This is probably a bug as I think this does not work with everything. Also I need New York, USA as well.
+"""
+
+birth_location=soup.find('a',title="Queens").text
+
+print(politician, birth_full_name,date_of_birth,birth_location,political_party)
