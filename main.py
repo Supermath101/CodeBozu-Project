4d22eb0a329da48df52e6a4d06ce449fa2a8b679
Tanishqqq19 <tanmeup195@gmail.com>
2021-12-10 22:18:32 +0300

    I have put political party as also a thing
    Political party has been added. I although need help to make the data presentable

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ import re
 html_file=requests.get('https://en.wikipedia.org/wiki/Donald_Trump')
 soup = BeautifulSoup(html_file.text, "lxml")
 
+# Birthday
 print("Birthday: " + soup.find(class_="bday").text)
 
-# print(soup.find(string="Spouse(s)").parent.parent.prettify())
+# Spouse
+spouses = []
 
-mylist=[]
 container=soup.find(string="Spouse(s)")
-print(container)
+for maybe_name in container.parent.parent.find(class_="infobox-data").stripped_strings:
+    for name in re.findall("^.+ .+$", maybe_name, re.M):
+        spouses.append(name)
 
-print(container.parent.parent.find(class_="infobox-data").text)
+print("Spouses: " + ", ".join(spouses))
+# Political Party
+political_party=soup.find('div',class_='plainlist')
+print(political_party.text)
