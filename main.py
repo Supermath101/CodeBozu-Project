669072e81133f441b800828dcf6bedaf26470a67
Supermath101 <>
2021-12-11 16:24:45 -0500

    Reorganized the fetching of data

--- main.py
+++ main.py
@@ -3,36 +3,36 @@ import requests
 import re
 
 def soup_to_infobox_data(keyword):
-    return keyword.parent.parent.find(class_="infobox-data").stripped_strings
+    return keyword.parent.parent.find(class_="infobox-data").get_text().splitlines()
 
 html_file=requests.get('https://en.wikipedia.org/wiki/Donald_Trump')
 soup = BeautifulSoup(html_file.text, "lxml")
 
-# Birthday
-date_of_birth=soup.find(class_="bday").text
-# print("Birthday: " + soup.find(class_="bday").text)
+# Birth details
+
 """
-We need it in this type of format 
+We need the birthday in this type of format 
 July 15,1975
 """
+
+birth_towns = []
+
 birth_dates = [] # Just in case we find multiple birth dates, lets display all of them.
 
-for maybe_birth_date_container in soup.find_all(string="Born"):
-    for maybe_birth_date in soup_to_infobox_data(maybe_birth_date_container):
-        for birth_date in re.findall("[A-Z][a-z]+ \d{1,2}, \d{4}", maybe_birth_date, re.M):
+for maybe_birth_container in soup.find_all(string="Born"):
+    for maybe_birth in soup_to_infobox_data(maybe_birth_container):
+        for birth_date in re.findall("[A-Z][a-z]+ \d{1,2}, \d{4}", maybe_birth):
             birth_dates.append(birth_date)
-# print(birth_dates)
-for i in birth_dates:
-    date_of_birth=i
+        for birth_town in re.findall("[A-z. ]+, [A-z. ]+, [A-z. ]+", maybe_birth):
+            birth_towns.append(birth_town)
+
 # Spouse
 spouses = []
 
-container=soup.find(string="Spouse(s)")
-for maybe_name in container.parent.parent.find(class_="infobox-data").stripped_strings:
-    for name in re.findall("^.+ .+$", maybe_name, re.M):
-        spouses.append(name)
-
-# print("Spouses: " + ", ".join(spouses))
+for maybe_name_container in soup.find_all(string="Spouse(s)"):
+    for maybe_name in soup_to_infobox_data(maybe_name_container):
+       for name in re.findall("^[A-z]+ [A-z]+", maybe_name):
+            spouses.append(name)
 
 
 # Political Party
@@ -40,22 +40,20 @@ parties = [] # Just in case we find multiple birth dates, lets display all of th
 
 for maybe_party_container in soup.find_all(string="Political party"):
     for maybe_party in soup_to_infobox_data(maybe_party_container):
-        for party in re.findall("[A-Z][a-z]+", maybe_party, re.M):
+        for party in re.findall("[A-Z][a-z]+", maybe_party):
             parties.append(party)
 
 political_party = ", ".join(parties)
 
-# Politicians
-politician=soup.find('div',class_='fn').text
-
 # Birth Full name
-birth_full_name=soup.find('div',class_="nickname").text
+birth_full_name=soup.find(class_="nickname").text
 
 # Birth Location
 """
 This is probably a bug as I think this does not work with everything. Also I need New York, USA as well.
 """
 
-birth_location=soup.find('a',title="Queens").text
+birth_location=", ".join(birth_towns)
+date_of_birth=", ".join(birth_dates)
 
-print(politician, birth_full_name,date_of_birth,birth_location,political_party)
+print(birth_full_name,date_of_birth,birth_location,political_party, ", ".join(spouses))
