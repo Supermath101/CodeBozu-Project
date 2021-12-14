5fedf351937703ae649ebbf93a221c72790f505f
Tanishqqq19 <tanmeup195@gmail.com>
2021-12-14 21:15:30 +0300

    i got the links, now we need to adapt the code like we did for trump

--- main.py
+++ main.py
@@ -2,80 +2,70 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-# def soup_to_infobox_data(keyword):
-#     return keyword.parent.parent.find(class_="infobox-data").get_text().splitlines()
+def soup_to_infobox_data(keyword):
+    return keyword.parent.parent.find(class_="infobox-data").get_text().splitlines()
 
 
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import re
 
-req = Request("https://en.wikipedia.org/wiki/Category:21st-century_presidents_of_the_United_States")
+req = Request("https://en.wikipedia.org/wiki/Category:21st-century_vice_presidents_of_the_United_States")
 html_page = urlopen(req)
 
 soup = BeautifulSoup(html_page, "lxml")
-m=soup.find(class_="mw-content-ltr")
-print(m.prettify())
-
-
-
-
-
-
-
+whole_page=soup.find(class_="mw-content-ltr")
+links_f=whole_page.find(class_="mw-content-ltr")
 
 links = []
-for link in m.findAll('a'):
-    links.append(link.get('href'))
+for link in links_f.findAll('a'):
+    getting_link="https://en.wikipedia.org"+link.get('href')
+    links.append(getting_link)
 
 print(links)
 
 
 # Birth details
-# """
-# We need the birthday in this type of format 
-# July 15,1975
-# """
+for i in links:
+    birth_towns = []
 
-# birth_towns = []
+    birth_dates = [] # Just in case we find multiple birth dates, lets display all of them.
 
-# birth_dates = [] # Just in case we find multiple birth dates, lets display all of them.
+    for maybe_birth_container in soup.find_all(string="Born"):
+        for maybe_birth in soup_to_infobox_data(maybe_birth_container):
+            for birth_date in re.findall("[A-Z][a-z]+ \d{1,2}, \d{4}", maybe_birth):
+                birth_dates.append(birth_date)
+            for birth_town in re.findall("[A-z. ]+, [A-z. ]+, [A-z. ]+", maybe_birth):
+                birth_towns.append(birth_town)
 
-# for maybe_birth_container in soup.find_all(string="Born"):
-#     for maybe_birth in soup_to_infobox_data(maybe_birth_container):
-#         for birth_date in re.findall("[A-Z][a-z]+ \d{1,2}, \d{4}", maybe_birth):
-#             birth_dates.append(birth_date)
-#         for birth_town in re.findall("[A-z. ]+, [A-z. ]+, [A-z. ]+", maybe_birth):
-#             birth_towns.append(birth_town)
+    # Spouse
+    spouses = []
 
-# # Spouse
-# spouses = []
-
-# for maybe_name_container in soup.find_all(string="Spouse(s)"):
-#     for maybe_name in soup_to_infobox_data(maybe_name_container):
-#        for name in re.findall("^[A-z]+ [A-z]+", maybe_name):
-#             spouses.append(name)
+    for maybe_name_container in soup.find_all(string="Spouse(s)"):
+        for maybe_name in soup_to_infobox_data(maybe_name_container):
+            for name in re.findall("^[A-z]+ [A-z]+", maybe_name):
+                    spouses.append(name)
 
 
-# # Political Party
-# parties = [] # Just in case we find multiple birth dates, lets display all of them.
+    # Political Party
+    parties = [] # Just in case we find multiple birth dates, lets display all of them.
 
-# for maybe_party_container in soup.find_all(string="Political party"):
-#     for maybe_party in soup_to_infobox_data(maybe_party_container):
-#         for party in re.findall("[A-Z][a-z]+", maybe_party):
-#             parties.append(party)
+    for maybe_party_container in soup.find_all(string="Political party"):
+        for maybe_party in soup_to_infobox_data(maybe_party_container):
+            for party in re.findall("[A-Z][a-z]+", maybe_party):
+                parties.append(party)
 
-# political_party = ", ".join(parties)
+    political_party = ", ".join(parties)
 
-# # Birth Full name
-# birth_full_name=soup.find(class_="nickname").text
+    # Birth Full name
+    birth_full_name=soup.find(class_="nickname").text
 
-# # Birth Location
-# """
-# This is probably a bug as I think this does not work with everything. Also I need New York, USA as well.
-# """
+    # Birth Location
+    """
+    This is probably a bug as I think this does not work with everything. Also I need New York, USA as well.
+    """
 
-# birth_location=", ".join(birth_towns)
-# date_of_birth=", ".join(birth_dates)
+    birth_location=", ".join(birth_towns)
+    date_of_birth=", ".join(birth_dates)
 
-# print(birth_full_name,date_of_birth,birth_location,political_party)
+    print(birth_full_name,date_of_birth,birth_location,political_party)
