97bcf5f4c17994cb195841cdf8788d7a49b28e02
Tanishqqq19 <tanmeup195@gmail.com>
2021-12-23 10:40:04 +0300

    i organized everything
    i m left to run other presidents and vice presidents

--- Bio_Data.py
+++ Bio_Data.py
@@ -1,59 +1,54 @@
 import pandas as pd
 from csv import writer
+# import csv
 import re
 
-def csv_file(full_name, birth_date, birth_place, party):
-    df=pd.DataFrame({'birth_name':[full_name], 'date of birth':[birth_date], 'birth location':[birth_place], "political party":[party]})
-    df.to_csv('information.csv',mode='a', header=False, index=False)
-    mylist=[full_name,birth_date,birth_place,party]
+def soup_to_infobox_data(keyword):
+    return keyword.parent.parent.find(class_="infobox-data").get_text().splitlines()
 
-def bio_data(soup, soup_to_infobox_data):
+def csv_file(soup):
+    df=pd.DataFrame({'birth_name':[birth_full_name(soup)], 'date of birth':[date_of_birth(soup)], 'birth location':[birth_location(soup)], "political party":[political_party(soup)]})
+    df.to_csv('trials.csv',mode='a', header=False, index=False)
 
-    # Birth details
-    birth_towns = [] 
-
-    birth_dates = [] # Just in case we find multiple birth dates, lets display all of them.
-
+def date_of_birth(soup):
+    birth_dates = []
     for maybe_birth_container in soup.find_all(string="Born"):
-        # print(soup_to_infobox_data(maybe_birth_container))
         for maybe_birth in soup_to_infobox_data(maybe_birth_container):
             for birth_date in re.findall("[A-Z][a-z]+ \d{1,2}, \d{4}", maybe_birth):
                 birth_dates.append(birth_date)
+    date_of_birth=", ".join(birth_dates)
+    return date_of_birth
+
+def birth_location(soup):
+    birth_towns = [] 
+    for maybe_birth_container in soup.find_all(string="Born"):
+        for maybe_birth in soup_to_infobox_data(maybe_birth_container):
             first=re.sub('(.*?)(\d)', '', maybe_birth)
             if first[0]==")":
                 birth_towns.append(first[1::])
             else:
                 birth_towns.append(first)
+    birth_location=", ".join(birth_towns)
+    return birth_location
 
-    # Spouse
+def spouses(soup):
     spouses = []
-
     for maybe_name_container in soup.find_all(string="Spouse(s)"):
         for maybe_name in soup_to_infobox_data(maybe_name_container):
             for name in re.findall("^[A-z]+ [A-z]+", maybe_name):
                     spouses.append(name)
+    return spouses
 
+def political_party(soup):
     # Political Party
     parties = [] # Just in case we find multiple parties, lets display all of them.
-
     for maybe_party_container in soup.find_all(string="Political party"):
         for maybe_party in soup_to_infobox_data(maybe_party_container):
             for party in re.findall("[A-Z][a-z]+", maybe_party):
                 parties.append(party)
-
     political_party = ", ".join(parties)
+    return political_party
 
-    # Birth Full name
-    # Try to get a header than personal info
-    # regex for (parenthesis)
-    birth_full_name=soup.find(id="firstHeading").text
-    for name in re.findall("[A-z \\.]+", birth_full_name):
-        full_birth_name=name
-    
-
-    birth_location=", ".join(birth_towns)
-    date_of_birth=", ".join(birth_dates)
-    
-    #creating csv file
-    csv_file(birth_full_name, date_of_birth, birth_location, political_party)
-
+def birth_full_name(soup):
+    birth_name=soup.find(id="firstHeading").text
+    return birth_name
--- main.py
+++ main.py
@@ -4,13 +4,10 @@ import requests
 import re
 import Bio_Data
 
-def soup_to_infobox_data(keyword):
-    return keyword.parent.parent.find(class_="infobox-data").get_text().splitlines()
-
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
-req = Request("https://en.wikipedia.org/wiki/Category:19th-century_vice_presidents_of_the_United_States")
+req = Request("https://en.wikipedia.org/wiki/Category:21st-century_vice_presidents_of_the_United_States")
 html_page = urlopen(req)
 
 soup = BeautifulSoup(html_page, "lxml")
@@ -26,6 +23,4 @@ for link in links_f.findAll('a'):
 for i in links:
     html_file = requests.get(i)
     soup=BeautifulSoup(html_file.text,"lxml")
-
-    #Calling bio_data() from Bio_Data.py
-    Bio_Data.bio_data(soup,soup_to_infobox_data)
+    Bio_Data.csv_file(soup)
