e45efca50b0fa47563c866f8d45b0fad29f4e21a
AnnaZ25 <96466596+AnnaZ25@users.noreply.github.com>
2021-12-29 20:10:12 +0300

    Extracting the info
    CSV file still needs to be created

--- Part 2.py
+++ Part 2.py
@@ -1,62 +1,71 @@
-import pandas as pd
 from bs4 import BeautifulSoup
+import pandas as pd
 from urllib.request import Request, urlopen
 import re
-
-#def csv_file(soup):
-#    df=pd.DataFrame({'Things':[things(soup)], 'Move':[move(soup)], 'Impact':[impact(soup)], "Upshot":[upshot(soup)]})
-#    df.to_csv('politico.csv',mode='a', header=False, index=False)
-
 
 req = Request("https://www.politico.com/news/magazine/2021/01/18/trump-presidency-administration-biggest-impact-policy-analysis-451479")
 html_page = urlopen(req)
 soup = BeautifulSoup(html_page, "lxml")
 
-body_text_info = soup.find_all('p', class_="story-text__paragraph")
+body_text_info = soup.find_all(class_=re.compile("story-text__"))
 
-#Extracting the things done
-def things(soup):
-    things_done = []
-    for thing in soup.find_all('h3', class_="story-text__heading-medium label"):
-        #adding each string to the array
-        things_done.append(thing.text)
-    return things_done #returns an ARRAY
+class section: 
+    def __init__(self, name):
+        self.name = name
+        self.subsections = []
 
-#Extracting the moves made
-def move(info):
-    move_done = [] 
-    for bit in info:
-        result = re.search(r"(?:The move:)(.*)", bit.text)
-        if result != None:
-            #adding each string to the array
-            move_done.append(result.group().replace("The move: ", ''))
-    return move_done #returns an ARRAY
- 
-#problem - one bit contains a couple of 'The move:' s and it is placed separately into the array, not grouped together.
+class subsection:
+    def __init__(self, name, text):
+        self.name = name
+        self.text = [text]
+
+current_subsection = ''
+current_section = ''
+sections = []
+
+for paragraph in body_text_info:
+    item = paragraph.text
+    if paragraph.name == "h3":
+        if current_subsection != '' and current_section != '':
+            current_section.subsections.append(current_subsection)
+            current_subsection = ''
+            sections.append(current_section)
+        current_section = section(item)
+
+    elif re.search(r"(?:The move:)(.*)", item):
+        if current_subsection != '':
+            current_section.subsections.append(current_subsection)
+        current_subsection = subsection("Move",item.replace("The move: ", ""))
+
+    elif re.search(r"(?:The impact:)(.*)", item):
+        if current_subsection != '':
+            current_section.subsections.append(current_subsection)
+        current_subsection = subsection("Impact",item.replace("The impact: ", ""))
+
+    elif re.search(r"(?:The upshot:)(.*)", item):
+        if current_subsection != '':
+            current_section.subsections.append(current_subsection)
+        writer = paragraph.find('i')
+        current_subsection = subsection("Upshot",item.replace("The upshot: ", "").replace(writer.text,""))
+
+    else:
+        #stopping text after heading but before 'The move:' from being added
+        if current_subsection != '':
+            current_subsection.text.append(item)
+
+if current_subsection != '' and current_section != '':
+    current_section.subsections.append(current_subsection)
+    sections.append(current_section)
 
 
-#Extracting the impacts made
-def impact(info):
-    impact_done = [] 
-    for bit in info:
-        result = re.search(r"(?:The impact:)(.*)", bit.text)
-        if result != None:
-            #adding each string to the array.
-            impact_done.append(result.group().replace("The impact: ", ''))
-    return impact_done #returns an ARRAY
+def csv_file():
+    df=pd.DataFrame({'Things':[thing], 'Move':[move], 'Impact':[impact], "Upshot":[upshot]})
+    df.to_csv('politico.csv',mode='a', header=False, index=False)
 
-#Extracting the upshots made
-def upshot(info):
-    upshot_done = [] 
-    for bit in info:
-        result = re.search(r"(?:The upshot:)(.*)", bit.text)
-        if result != None:
-            #Finding the writers name in the data
-            writer = bit.find('i').text
-            #cleaning each string up and adding it to the array.
-            upshot_done.append(result.group().replace("The upshot: ", '').strip(writer))
-    return upshot_done #returns ARRAY
-        
-#printing the upshots_done array (testing the program)
-print(upshot(body_text_info))
-#csv_file(soup)
+for section in sections:
+    thing = section.name
+    list = []
+    for subsection in section.subsections:
+        sub = subsection.text
+        #add code to allocate the correct bits to the variables 'move', 'impact' and 'upshot'
+        #csv_file()
