9032ef8f0872e9c23bc6445e2b6e83d880584a0c
Tanishqqq19 <tanmeup195@gmail.com>
2021-12-29 18:35:26 +0300

    i have all the presidents from the east coast

--- insights.py
+++ insights.py
@@ -4,39 +4,25 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
-req = Request('https://en.wikipedia.org/wiki/East_Coast_of_the_United_States')
-html_page = urlopen(req)
-def soup_to_infobox_data(keyword):
-    return keyword.parent.parent.find(class_="infobox-data")
 
-soup = BeautifulSoup(html_page, "lxml")
-cities=[]
-east_coast_cities=[]
-for i in soup.find_all(string="Principal cities"):
-    for maybe_birth in soup_to_infobox_data(i):
-        cities.append(maybe_birth)
-while ' ' in cities:
-    cities.remove(' ')
-for i in cities:
-    east_coast_cities.append(i.text)
-while '' in east_coast_cities:
-    east_coast_cities.remove('')
-print(east_coast_cities)
+east_coast_states=["Maine", "New Hampshire", "Massachusetts", "Rhode Island", "Connecticut", "New York", "New Jersey", "Delaware", "Maryland", "Virginia", "North Carolina", "South Carolina", "Georgia", "Florida"]
 
 file = open('unique_information.csv')
 csvreader = csv.reader(file)
 birth_area=[]
 all_the_people = []
+birth_states=[]
 people_from_east_coast=0
 for row in csvreader:
     all_the_people.append(row)
 for birth in all_the_people:
-    for cities in east_coast_cities:
-        births=birth[2]
-        while ',' in births:
-            births=births.replace(",","")
-        if cities in births:
-            print(cities,birth[0])
+    for cities in east_coast_states:
+        if cities in birth[2]:
+            # print(cities,birth[0])
+            birth_states.append(cities)
             people_from_east_coast+=1
+            continue
     # birth_area.append(births[2])
 print(people_from_east_coast)
+print('----------------')
+print(set(birth_states))
