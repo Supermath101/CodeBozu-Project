b7a51366e8edb756697d6ce9be0990e2101b415d
Tanishqqq19 <tanmeup195@gmail.com>
2021-12-29 19:26:38 +0300

    insights finished

--- insights.py
+++ insights.py
@@ -6,23 +6,24 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
 east_coast_states=["Maine", "New Hampshire", "Massachusetts", "Rhode Island", "Connecticut", "New York", "New Jersey", "Delaware", "Maryland", "Virginia", "North Carolina", "South Carolina", "Georgia", "Florida"]
+west_coast_states=["California","Oregon","Washington","Alaska","Hawaii"]
 
 file = open('unique_information.csv')
 csvreader = csv.reader(file)
 birth_area=[]
 all_the_people = []
-birth_states=[]
 people_from_east_coast=0
+people_from_west_coast=0
 for row in csvreader:
     all_the_people.append(row)
 for birth in all_the_people:
     for cities in east_coast_states:
         if cities in birth[2]:
-            # print(cities,birth[0])
-            birth_states.append(cities)
             people_from_east_coast+=1
-            continue
-    # birth_area.append(births[2])
+
+for birth in all_the_people:
+    for cities in west_coast_states:
+        if cities in birth[2]:
+            people_from_west_coast+=1
 print(people_from_east_coast)
-print('----------------')
-print(set(birth_states))
+print(people_from_west_coast)
