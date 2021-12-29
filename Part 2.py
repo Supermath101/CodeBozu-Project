cdf62237723cc4a7c75132dd0a95179fa6d010a6
AnnaZ25 <96466596+AnnaZ25@users.noreply.github.com>
2021-12-29 13:43:26 +0300

    Fixed the main bug, new issue found
    One section in the article has a mistake (I think) - it has 3 'The move:' sections and no impact or upshot sections, while this program separates all the 'The move:' bits and does not have this data grouped with no way of knowing that this was an error.

--- Part 2.py
+++ Part 2.py
@@ -11,35 +11,52 @@ import re
 req = Request("https://www.politico.com/news/magazine/2021/01/18/trump-presidency-administration-biggest-impact-policy-analysis-451479")
 html_page = urlopen(req)
 soup = BeautifulSoup(html_page, "lxml")
-#csv_file(soup)
 
 body_text_info = soup.find_all('p', class_="story-text__paragraph")
 
-#things done works
+#Extracting the things done
 def things(soup):
     things_done = []
     for thing in soup.find_all('h3', class_="story-text__heading-medium label"):
+        #adding each string to the array
         things_done.append(thing.text)
     return things_done #returns an ARRAY
 
-#move doesnt work
-#def move(info):
-#    move_done = [] 
-#    for bit in info:
-#        if bit.find(string="The move:") != None:
-#            print(bit) #still prints the non-containing "The move:" bits; do not know why
-#            move_done.append(bit.text.strip("The move:"))
+#Extracting the moves made
+def move(info):
+    move_done = [] 
+    for bit in info:
+        result = re.search(r"(?:The move:)(.*)", bit.text)
+        if result != None:
+            #adding each string to the array
+            move_done.append(result.group().replace("The move: ", ''))
+    return move_done #returns an ARRAY
+ 
+#problem - one bit contains a couple of 'The move:' s and it is placed separately into the array, not grouped together.
 
-#once move is corrected format can be copied for these as 'info' variable contains these bits as well
 
-#def impact(soup):
-#    impact_done = []
-#    for impact in soup.find_all('h3', class_="story-text__heading-medium label"):
-#        impact_done.append(impact.text)
+#Extracting the impacts made
+def impact(info):
+    impact_done = [] 
+    for bit in info:
+        result = re.search(r"(?:The impact:)(.*)", bit.text)
+        if result != None:
+            #adding each string to the array.
+            impact_done.append(result.group().replace("The impact: ", ''))
+    return impact_done #returns an ARRAY
 
-#def upshot(soup):
-#    upshot_done = []
-#    for upshot in soup.find_all('h3', class_="story-text__heading-medium label"):
-#        upshot_done.append(upshot.text)
-
-#print(move(body_text_info))
+#Extracting the upshots made
+def upshot(info):
+    upshot_done = [] 
+    for bit in info:
+        result = re.search(r"(?:The upshot:)(.*)", bit.text)
+        if result != None:
+            #Finding the writers name in the data
+            writer = bit.find('i').text
+            #cleaning each string up and adding it to the array.
+            upshot_done.append(result.group().replace("The upshot: ", '').strip(writer))
+    return upshot_done #returns ARRAY
+        
+#printing the upshots_done array (testing the program)
+print(upshot(body_text_info))
+#csv_file(soup)
