b6c9d197f9b1761f4e35845f22b1977c1f6081e9
Tanishqqq19 <tanmeup195@gmail.com>
2022-01-04 21:45:41 +0300

    I fixed the bug by code but caused another one
    fix the bug

--- Part 2.py
+++ Part 2.py
@@ -8,7 +8,7 @@ html_page = urlopen(req)
 soup = BeautifulSoup(html_page, "lxml")
 
 body_text_info = soup.find_all(class_=re.compile("story-text__"))
-
+lists=[]
 class section: 
     def __init__(self, name):
         self.name = name
@@ -58,10 +58,9 @@ if current_subsection != '' and current_section != '':
 for section in sections:
     thing = section.name
     statement = thing
+    lists.append(statement)
     for subsection in section.subsections:
-        statement += ", " + ''.join(subsection.text)
-        #problem, commas in the text will mess up the csv file. 
-        #Need to add quotation marks around the large pieces of text 
-        #Also need use pandas to make the csv file instead 
-    df=pd.DataFrame({'everything':[statement]})
-    df.to_csv('politico.csv',mode='a', header=False, index=False)
+        lists.append(subsection.text)
+    composite_list=[lists[x:x+4]for x in range(0,len(lists),4)]
+df=pd.DataFrame(composite_list,columns=["Things","Move","Impact","Upshot"])
+df.to_csv('politico.csv',mode='a',index=False)
