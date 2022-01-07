ac71b30f3a279111e6d288cfe4ab381dacd9fa63
AnnaZ25 <96466596+AnnaZ25@users.noreply.github.com>
2022-01-07 19:48:27 +0300

    works
     - might find better links for Obama and Bush to scrape tho

--- info_from_bbc.py
+++ info_from_bbc.py
@@ -35,19 +35,18 @@ def writing_to_file(title, body_text):
 
 
 links = ["https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump", "https://www.bbc.com/news/topics/czyymgqmn6dt/joe-biden",
- "https://www.bbc.com/news/topics/cvenzmgywl4t/barack-obama"]
-files = ["bbc_trump.csv", "bbc_obama.csv", "bbc_biden.csv"]
-
+ "https://www.bbc.com/news/topics/cvenzmgywl4t/barack-obama", "https://www.bbc.com/news/topics/cv0y3dkx5eet/george-w-bush"]
+files = ["bbc_trump.csv", "bbc_obama.csv", "bbc_biden.csv", "bbc_bush.csv"]
 
 
 for link_page in range(0, len(links)):
-    print(link_page)
+    print(links[link_page])
     html_page = urlopen(links[link_page])
     soup = bsoup(html_page, "lxml")
-    links = soup.findAll("a", class_="qa-heading-link lx-stream-post__header-link")
+    links_in_page = soup.findAll("a", class_="qa-heading-link lx-stream-post__header-link")
     
     with open(files[link_page], 'w', encoding='utf-8') as file:
-        for link in links:
+        for link in links_in_page:
             getting_link = "https://www.bbc.com" + link.get('href')
             html_page = requests.get(getting_link)
             soup = bsoup(html_page.text, "lxml")
@@ -61,10 +60,10 @@ for link_page in range(0, len(links)):
                 video_details = writing_to_file(title_vid, body_text_vid)        
     file.close()
 
-    # mean used to calculate the average positivity score 
+    # mean used to calculate the average positivity score for the current link page
     total_positivity = float(regular_details[0]) + float(video_details[0])
     count = int(regular_details[1]) + int(video_details[1])
 
     average = round(total_positivity/count,2) #float format
     average_string = str(average) + "%" #string format
-    print(link_page, average_string)
+    
