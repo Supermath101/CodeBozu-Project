0367f64cf2a47137886503a0e10943e4768a2c28
Tanishqqq19 <tanmeup195@gmail.com>
2022-01-07 11:56:14 +0300

    deleted

--- info_from_bbc.py
+++ info_from_bbc.py
@@ -9,8 +9,6 @@ link_page ="https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump"
 html_page = urlopen(link_page)
 soup = bsoup(html_page, "lxml")
 links_on_page = soup.findAll("a", class_ = "qa-heading-link lx-stream-post__header-link")
-<<<<<<< HEAD:info_from_bbc.py
-<<<<<<< HEAD:info_from_bbc.py
 '''
 for link in links_on_page:
     getting_link = "https://www.bbc.com/news" + link.get('href')
