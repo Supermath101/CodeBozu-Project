08325187d6e207046588b8c57b522691a227b119
AnnaZ25 <96466596+AnnaZ25@users.noreply.github.com>
2022-01-07 19:28:39 +0300

    multiple links
    - problem - not all links are being scraped and there is an error

--- info_from_bbc.py
+++ info_from_bbc.py
@@ -33,31 +33,38 @@ def writing_to_file(title, body_text):
         count += 1
     return(total_positivity, count)
 
-link_page ="https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump"
-html_page = urlopen(link_page)
-soup = bsoup(html_page, "lxml")
-links = soup.findAll("a", class_="qa-heading-link lx-stream-post__header-link")
+
+links = ["https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump", "https://www.bbc.com/news/topics/czyymgqmn6dt/joe-biden",
+ "https://www.bbc.com/news/topics/cvenzmgywl4t/barack-obama"]
+files = ["bbc_trump.csv", "bbc_obama.csv", "bbc_biden.csv"]
 
 
-with open("bbc.csv", 'w', encoding='utf-8') as file:
-    for link in links:
-        getting_link = "https://www.bbc.com" + link.get('href')
-        html_page = requests.get(getting_link)
-        soup = bsoup(html_page.text, "lxml")
-        title_vid = soup.find("h1", class_="ssrcss-1qr3f1s-StyledHeading e1fj1fc10")
-        body_text_vid = soup.findAll("div", class_="ssrcss-1up5zkp-StyledSummary elwf6ac3")
-        title_reg = soup.find("h1", class_="ssrcss-gcq6xq-StyledHeading e1fj1fc10")
-        body_text_reg = soup.findAll("div",class_="ssrcss-uf6wea-RichTextComponentWrapper e1xue1i85")    
-        if title_reg != None and body_text_reg != None:
-            regular_details = writing_to_file(title_reg, body_text_reg)
-        elif title_vid != None and body_text_vid != None:
-            video_details = writing_to_file(title_vid, body_text_vid)          
-file.close()
 
+for link_page in range(0, len(links)):
+    print(link_page)
+    html_page = urlopen(links[link_page])
+    soup = bsoup(html_page, "lxml")
+    links = soup.findAll("a", class_="qa-heading-link lx-stream-post__header-link")
+    
+    with open(files[link_page], 'w', encoding='utf-8') as file:
+        for link in links:
+            getting_link = "https://www.bbc.com" + link.get('href')
+            html_page = requests.get(getting_link)
+            soup = bsoup(html_page.text, "lxml")
+            title_vid = soup.find("h1", class_="ssrcss-1qr3f1s-StyledHeading e1fj1fc10")
+            body_text_vid = soup.findAll("div", class_="ssrcss-1up5zkp-StyledSummary elwf6ac3")
+            title_reg = soup.find("h1", class_="ssrcss-gcq6xq-StyledHeading e1fj1fc10")
+            body_text_reg = soup.findAll("div",class_="ssrcss-uf6wea-RichTextComponentWrapper e1xue1i85")    
+            if title_reg != None and body_text_reg != None:
+                regular_details = writing_to_file(title_reg, body_text_reg)
+            elif title_vid != None and body_text_vid != None:
+                video_details = writing_to_file(title_vid, body_text_vid)        
+    file.close()
 
-# mean used to calculate the average positivity score 
-total_positivity = float(regular_details[0]) + float(video_details[0])
-count = int(regular_details[1]) + int(video_details[1])
+    # mean used to calculate the average positivity score 
+    total_positivity = float(regular_details[0]) + float(video_details[0])
+    count = int(regular_details[1]) + int(video_details[1])
 
-average = round(total_positivity/count,2) #float format
-average_string = str(average) + "%" #string format
+    average = round(total_positivity/count,2) #float format
+    average_string = str(average) + "%" #string format
+    print(link_page, average_string)
