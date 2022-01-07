a78966c9ce75bd2f1445d0ed940bdb1530673049
Tanishqqq19 <tanmeup195@gmail.com>
2022-01-07 11:54:23 +0300

    tiny change to the csv file
    let this commit

--- info_from_bbc.py
+++ info_from_bbc.py
@@ -2,13 +2,14 @@ import pandas as pd
 from csv import writer
 import requests
 from bs4 import BeautifulSoup as bsoup
-from urllib.request import Request, urlopen
+from urllib.request import urlopen
 
 
 link_page ="https://www.bbc.com/news/topics/cp7r8vgl2lgt/donald-trump"
 html_page = urlopen(link_page)
 soup = bsoup(html_page, "lxml")
 links_on_page = soup.findAll("a", class_ = "qa-heading-link lx-stream-post__header-link")
+<<<<<<< HEAD:info_from_bbc.py
 '''
 for link in links_on_page:
     getting_link = "https://www.bbc.com/news" + link.get('href')
@@ -20,5 +21,16 @@ for link in links_on_page:
 getting_link = "https://www.bbc.com/news" + links_on_page[0].get('href')
 html_page = requests.get(getting_link)
 soup = bsoup(html_page.text, "lxml")
-body_text = 
+=======
 
+>>>>>>> f3637c01d080c5421e088cc894fe15a7f84a44d9:BBC.py
+file = open("BBC.csv", "w")
+for link in links_on_page:
+    getting_link = "https://www.bbc.com" + link.get('href')
+    html_page = requests.get(getting_link)
+    soup = bsoup(html_page.text, "lxml")
+    title = soup.find("h1", class_="ssrcss-gcq6xq-StyledHeading e1fj1fc10").text
+    body_text = soup.findAll("div",class_="ssrcss-uf6wea-RichTextComponentWrapper e1xue1i85").text
+    statement = title + ", " + body_text + "\n"
+    file.write(statement)
+file.close()
