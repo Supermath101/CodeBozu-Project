78886c4061e8ec0a6d21221f6fc6fbefb22b45f8
Tanishqqq19 <tanmeup195@gmail.com>
2022-01-10 09:51:46 +0300

    washington post on trump works perfectly

--- info_from_washington_post.py
+++ info_from_washington_post.py
@@ -12,18 +12,19 @@ mylist=[]
 for link in m:
     if 'href' in link.attrs:
         mj=str(link.attrs['href'])
-        if "https://www.washingtonpost.com/politics/" in mj and "https://www.washingtonpost.com/politics/"!=mj:
+        if "https://www.washingtonpost.com/politics/" in mj and "https://www.washingtonpost.com/politics/"!=mj and "https://www.washingtonpost.com/politics/courts-law/"!=mj:
             mylist.append(mj)
-        if "https://www.washingtonpost.com/immigration/" in mj:
+        if "https://www.washingtonpost.com/immigration/" in mj and "https://www.washingtonpost.com/immigration/"!=mj:
             mylist.append(mj)
-        if "https://www.washingtonpost.com/national-security/" in mj:
+        if "https://www.washingtonpost.com/national-security/" in mj and "https://www.washingtonpost.com/national-security/":
             mylist.append(mj)
 all_the_links = []
 for x in mylist:
     if x not in all_the_links:
         all_the_links.append(x)
 print(all_the_links)
-with open('washington_post_on_trump', 'w', encoding='utf-8') as file:
+print(len(all_the_links))
+with open('washington_post_on_trump.csv', 'w', encoding='utf-8') as file:
     for i in all_the_links:
         req = Request(i)
         html_page = urlopen(req)
@@ -33,8 +34,8 @@ with open('washington_post_on_trump', 'w', encoding='utf-8') as file:
         for i in j:
             l=i.text
             print(l)
-            print(type(l))
             file.write(str(l))
+            file.write('\n')
 file.close()
 
 """
