6a27a786ddf110be3cbf0fc0fd12f7dc0cd5b3e7
AnnaZ25 <96466596+AnnaZ25@users.noreply.github.com>
2022-01-10 19:38:21 +0300

    Huffington Post - still need to clean data for all websites

--- heatmap.py
+++ heatmap.py
@@ -9,8 +9,9 @@ file = open("heatmap_data.csv", "r")
 for line in file:
     separated = line.split(", ")
     bbc = separated[1].strip("%")
-    forbes = separated[2].strip("\n")
-    list = [float(bbc), float(forbes.strip("%"))]
+    forbes = separated[2].strip("%")
+    huff_post = separated[3].strip("\n")
+    list = [float(bbc), float(forbes), float(huff_post.strip("%"))]
     array.append(list)
 file.close()
 
@@ -18,10 +19,9 @@ file.close()
 df_cm = pd.DataFrame(array)
 
 sn.set(font_scale = 1.0)
-svm = sn.heatmap(df_cm, annot = True, cmap = 'YlGnBu', yticklabels = ["Donald Trump", "Joe Biden", "Barack Obama", "George W. Bush", "Bill Clinton"], xticklabels = ["BBC", "Forbes"])
+svm = sn.heatmap(df_cm, annot = True, cmap = 'YlGnBu', yticklabels = ["Donald Trump", "Joe Biden", "Barack Obama", "George W. Bush", "Bill Clinton"], xticklabels = ["BBC", "Forbes", "Huffington Post"])
 plt.rcParams["figure.autolayout"] = True
 plt.tight_layout()
-plt.title("Favourability of Politicians")
-
+plt.title("Politician Favourability by News Media")
 figure = svm.get_figure()
-figure.savefig('Favourability of Politicians.png', dpi = 200)
+figure.savefig('Politician Favourability by News Media.png', dpi = 200)
