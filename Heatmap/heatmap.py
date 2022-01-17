48df77ab3c9edebd6a21bdb94f8110cb8428dc63
AnnaZ25 <96466596+AnnaZ25@users.noreply.github.com>
2022-01-17 09:17:13 +0300

    Heatmap insights

--- Heatmap/heatmap.py
+++ Heatmap/heatmap.py
@@ -5,7 +5,7 @@ from pylab import savefig
 import os 
 
 # Data to heatmap
-files = ["averages_bbc.txt", "averages_forbes.txt", "averages_huff_post.txt", "averages_fox_news.txt"]
+files = ["averages_bbc.txt", "averages_forbes.txt", "averages_fox_news.txt", "averages_huff_post.txt"]
 array_name = []
 array_data = []
 folder = "Heatmap"
@@ -42,9 +42,9 @@ for line in file:
     separated = line.split(", ")
     bbc = separated[1].strip("%")
     forbes = separated[2].strip("%")
-    huff_post=separated[3].strip("%")
-    fox_news=separated[4].strip("\n")
-    list = [float(bbc), float(forbes),float(huff_post),float(fox_news.strip('%'))]
+    fox_news=separated[3].strip("%")
+    huff_post=separated[4].strip("\n")
+    list = [float(bbc), float(forbes),float(fox_news),float(huff_post.strip('%'))]
     array.append(list)
 file.close()
 
@@ -52,7 +52,7 @@ file.close()
 df_cm = pd.DataFrame(array)
 
 sn.set(font_scale = 1.5)
-svm = sn.heatmap(df_cm, annot = True, cmap = 'YlGnBu', yticklabels = ["Donald Trump", "Joe Biden", "Barack Obama", "George W. Bush", "Bill Clinton"], xticklabels = ["BBC", "Forbes", "Huffington Post", "Fox News"])
+svm = sn.heatmap(df_cm, annot = True, cmap = 'YlGnBu', yticklabels = ["Donald Trump", "Joe Biden", "Barack Obama", "George W. Bush", "Bill Clinton"], xticklabels = ["BBC", "Forbes", "Fox News", "Huffington Post"])
 plt.rcParams["figure.autolayout"] = True
 plt.tight_layout()
 plt.title("Politican Favourability by News Media")
